746a9a9fba664dc4a484fe00a95928c94ef565a6
YevaUsatova <yeva.usatova@gmail.com>
2021-03-08 00:37:17 -0500

    commit tree b4ac3b563482929be91622c2a494827b944ff3c3

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+    block_return_values = []
+    i = 0
+    while i < collection.length
+        block_return_values.push yield (collection[i])
+        i = i+1
+    end
+    if block_return_values.include?(false)
+        false
+     else
+        true
+    end
 end
+block_return_values = [true, false, false] 
+my_all?([1,2,3]) {|i| i < 2}
